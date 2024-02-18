4b9ea7f43e84fabb8a9f23a170b59ece6247f3ec
devHoangDong <60665278+devHoangDong@users.noreply.github.com>
2024-02-18 16:04:32 +0700

    18/2

--- Assets/Data/Item/Inventory/Inventory.cs
+++ Assets/Data/Item/Inventory/Inventory.cs
@@ -18,14 +18,98 @@ public class Inventory : SaiMonoBehaviour
 
     public virtual bool AddItem(ItemCode itemCode, int addCount)
     {
-        ItemInventory itemInventory = this.GetItemByCode(itemCode);
 
-        int newCount = itemInventory.itemCount + addCount;
-        if (newCount > itemInventory.maxStack) return false;
-        itemInventory.itemCount = newCount;
+        ItemProfileSO itemProfile = this.GetItemProfile(itemCode);
+
+        int addRemain = addCount;
+        int newCount;
+        int itemMaxStack;
+        int addMore;
+        ItemInventory itemExist;
+        for (int i = 0; i < this.maxSlot; i++)
+        {
+            itemExist = this.GetItemNotFullStack(itemCode);
+            if (itemExist == null)
+            {
+                if (this.IsInventoryFull()) return false;
+
+                itemExist = this.CreateEmptyItem(itemProfile);
+                this.items.Add(itemExist);
+            }
+
+            newCount = itemExist.itemCount + addRemain;
+
+            itemMaxStack = this.GetMaxStack(itemExist);
+            if (itemMaxStack > 0 && newCount > itemMaxStack)
+            {
+                addMore = itemMaxStack - itemExist.itemCount;
+                newCount = itemExist.itemCount + addMore;
+                addRemain -= addMore;
+            }
+            else
+            {
+                addRemain -= newCount;
+            }
+
+            itemExist.itemCount = newCount;
+            if (addRemain < 1) break;
+        }
+
         return true;
     }
 
+    protected virtual bool IsInventoryFull()
+    {
+        if (this.items.Count >= this.maxSlot) return true;
+        return false;
+    }
+
+    protected virtual ItemProfileSO GetItemProfile(ItemCode itemCode)
+    {
+        var profiles = Resources.LoadAll("Item", typeof(ItemProfileSO));
+        foreach (ItemProfileSO profile in profiles)
+        {
+            if (profile.itemCode != itemCode) continue;
+            return profile;
+        }
+        return null;
+    }
+
+    protected virtual ItemInventory GetItemNotFullStack(ItemCode itemCode)
+    {
+        foreach(ItemInventory itemInventory in this.items)
+        {
+            if (itemCode != itemInventory.itemProfile.itemCode) continue;
+            if (this.IsFullStack(itemInventory)) continue;
+            return itemInventory;
+        }
+        return null;
+    }
+
+    protected virtual bool IsFullStack(ItemInventory itemInventory)
+    {
+        if (itemInventory == null) return true;
+        int maxStack = this.GetMaxStack(itemInventory);
+        return itemInventory.itemCount >= maxStack;
+    }
+
+    protected virtual int GetMaxStack(ItemInventory itemInventory)
+    {
+        if (itemInventory == null) return 0;
+
+        return itemInventory.maxStack;
+    }
+
+    protected virtual ItemInventory CreateEmptyItem(ItemProfileSO itemProfile)
+    {
+        ItemInventory itemInventory = new ItemInventory
+        {
+            itemProfile = itemProfile,
+            maxStack = itemProfile.defaultMaxStack
+        };
+        return itemInventory;
+    }
+    
     public virtual bool DeductItem(ItemCode itemCode, int addCount)
     {
         ItemInventory itemInventory = this.GetItemByCode(itemCode);
